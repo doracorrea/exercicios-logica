e17c48b29933966315140989a422f1ac9e51fb8d
doracorrea <isadoraesther08@gmail.com>
2025-05-28 17:08:23 -0300

    Add files via upload

---
+++ 01-script.js
@@ -0,0 +1,13 @@
+// sq2 - ex 9 
+// mensagem secreta: peça ao usuário para digitar uma senha e, se a senha for "abracadabra", exiba uma mensagem secreta.
+
+
+let senha 
+
+senha = prompt  ("Digite uma senha:")
+    if (senha == "abacadabra") {
+    alert ("senha está correta, bem vindo ao mundo secreto")
+
+} else { alert ("senha incorreta")
+
+}
---
+++ 02-script.js
@@ -0,0 +1,12 @@
+ // sq3 - exercicio 3
+ // Verificando votação: peça ao usuario para inserir sua idade e verifique se ele é elegivel para votar ( idade minima 16 anos).
+
+ let idade 
+
+idade = prompt ("Informe sua idade:")
+if (idade >= 16) {
+    alert ("Idade legivel para votar")
+}
+else {
+    alert ("Volte quando for +16!!")
+} 
